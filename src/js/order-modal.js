51a75a17fc3e1b24e9fba3c0532bd66d7965c2ea
anna_borysova <anna@pavlyuk.org>
2025-12-13 19:11:29 +0200

    add open-modal html, css

---
+++ src/js/order-modal.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const formElem = document.querySelector("#order-modal-form");
+
+formElem.addEventListener ("submit", async e => {
+  e.preventDefault();
+  const { email, phone } = e.target.elements;
+  const formData = {
+    email: email.value,
+    phone: phone.value,
+    animalId
+  }
+});
+
+
+(() => {
+  const refs = {
+    // Додати атрибут data-modal-open на кнопку відкриття
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    // Додати атрибут data-modal-close на кнопку закриття
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    // Додати атрибут data-modal на бекдроп модалки
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs.modal.classList.toggle("is-open");
+  }
+})();
